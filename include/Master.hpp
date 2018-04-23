19c43a0a47dc996b3e77081fd4317039fbebb2c1
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-04-23 16:56:30 +0200

    Add master header : Creation of enum info

---
+++ include/Master.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2018
+** plazza
+** File description:
+** Master
+*/
+
+#ifndef PLAZZA_MASTER_HPP
+	#define PLAZZA_MASTER_HPP
+
+	namespace plazza {
+
+		enum Information {
+			PHONE_NB, EMAIL_ADDR, IP_ADDR
+		};
+
+		class Master {
+		public:
+			Master();
+			~Master();
+
+		};
+
+	};
+
+#endif
