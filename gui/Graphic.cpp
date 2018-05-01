fd0739fb9ba4829670c0c7aa17e92db95199e46a
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-05-01 22:55:55 +0200

    Graphic.cpp/hpp: Cut of my functions set Bow with command line Change Mode without change in window (Try to make Drag and Drop Box)

--- gui/Graphic.cpp
+++ gui/Graphic.cpp
@@ -9,29 +9,44 @@
 #include "Graphic.hpp"
 
 gfx::fileManager::fileManager() : m_Box(Gtk::ORIENTATION_VERTICAL),
+				  m_mode("Drag & Drop"),
 				  m_validate("Validate")
 {
-	set_default_size(1280, 720);
-	set_border_width(10);
-	m_validate.signal_clicked().connect(sigc::mem_fun(*this,
-							&fileManager::on_button_clicked));
+	setWindow();
 	add(m_Box);
-	m_Entry.set_max_length(258);
-	m_Box.pack_start(m_Entry, Gtk::PACK_SHRINK);
-	m_Box.pack_start(m_validate, Gtk::PACK_SHRINK);
-	m_Entry.set_icon_from_icon_name("edit-find");
-	m_Entry.signal_icon_press().connect(sigc::mem_fun(*this,
-							  &fileManager::on_icon_pressed));
-	m_validate.set_size_request(10, 10);
-	m_validate.show();
-	m_Entry.show();
-	set_title("plazza");
-	set_role("plazza");
+	setBoxInputCmdLine();
 	show_all_children();
 }
 
 gfx::fileManager::~fileManager()
 {
+}
+
+void gfx::fileManager::setWindow()
+{
+	set_default_size(1280, 720);
+	set_border_width(10);
+	set_title("plazza");
+	set_role("plazza");
+}
+
+void gfx::fileManager::setBoxInputCmdLine()
+{
+	m_Box.pack_start(m_Entry, Gtk::PACK_SHRINK);
+	m_Box.pack_start(m_mode, Gtk::PACK_SHRINK);
+	m_Box.pack_start(m_validate, Gtk::PACK_SHRINK);
+	m_mode.signal_clicked().connect(sigc::mem_fun(*this,
+							  &fileManager::changeMode));
+	m_validate.signal_clicked().connect(sigc::mem_fun(*this,
+							  &fileManager::on_button_clicked));
+	m_Entry.set_max_length(258);
+	m_Entry.set_icon_from_icon_name("edit-find");
+	m_Entry.signal_icon_press().connect(sigc::mem_fun(*this,
+							  &fileManager::on_icon_pressed));
+	m_validate.set_size_request(10, 10);
+	m_validate.show();
+	m_mode.show();
+	m_Entry.show();
 }
 
 void gfx::fileManager::on_icon_pressed(Gtk::EntryIconPosition iconPosition, const GdkEventButton* button)
@@ -44,6 +59,17 @@ void gfx::fileManager::on_icon_pressed(Gtk::EntryIconPosition iconPosition, cons
 void gfx::fileManager::on_button_clicked()
 {
 	std::cout << m_Entry.get_text() << std::endl;
+}
+
+void gfx::fileManager::changeMode()
+{
+	if (mode == CMD_LINE) {
+		m_mode.set_label("Drag & Drop");
+		mode = DRAG_AND_DROP;
+	} else if (mode == DRAG_AND_DROP) {
+		m_mode.set_label("Command Line");
+		mode = CMD_LINE;
+	}
 }
 
 int main(int ac, char **av)
--- include/Graphic.hpp
+++ include/Graphic.hpp
@@ -15,19 +15,30 @@
 
 	namespace gfx {
 
+		typedef enum e_CLI_Mode {
+			DRAG_AND_DROP,
+			CMD_LINE
+		} CLI_Mode;
+
 		class fileManager  : public Gtk::Window {
 		public:
 			fileManager();
 			virtual ~fileManager();
 
 			virtual void on_button_clicked();
-			virtual void on_icon_pressed(Gtk::EntryIconPosition, const GdkEventButton*);
+			virtual void changeMode();
+			virtual void on_icon_pressed(Gtk::EntryIconPosition, const GdkEventButton *);
 
 
 		protected:
 			Gtk::Box m_Box;
 			Gtk::Entry m_Entry;
+			Gtk::Button m_mode;
 			Gtk::Button m_validate;
+		private:
+			CLI_Mode mode = CMD_LINE;
+			void setWindow();
+			void setBoxInputCmdLine();
 		};
 	}
 
