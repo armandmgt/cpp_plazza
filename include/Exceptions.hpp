44bdd69c5bb268fcf805e37bb64028e093dda08d
armandmgt <armand.megrot@epitech.eu>
2018-05-02 20:25:39 +0200

    new RuntimeError exception class

--- include/Exceptions.hpp
+++ include/Exceptions.hpp
@@ -6,20 +6,33 @@
 */
 
 #ifndef PLAZZA_EXCEPTIONS_HPP
-#define PLAZZA_EXCEPTIONS_HPP
+	#define PLAZZA_EXCEPTIONS_HPP
 
-#include <exception>
+	#include <exception>
 
-class ArgumentError : public std::exception
-{
-public:
-	explicit ArgumentError(std::string const &message) : _msg(message) {}
-	const char *what() const noexcept override {
-		return _msg.c_str();
-	}
+	class ArgumentError : public std::exception
+	{
+	public:
+		explicit ArgumentError(std::string const &message)
+		: _msg(message) {}
+		const char *what() const noexcept override {
+			return _msg.c_str();
+		}
 
-protected:
-	std::string const _msg;
-};
+	protected:
+		std::string const _msg;
+	};
+
+	class RuntimeError : public std::runtime_error {
+	public:
+		explicit RuntimeError(std::string const &message)
+		: runtime_error(message), _msg(message) {}
+		const char *what() const noexcept override {
+			return _msg.c_str();
+		}
+
+	protected:
+		std::string const _msg;
+	};
 
 #endif //PLAZZA_EXCEPTIONS_HPP
