23697c372a9f1bbbefd0c0a38ef0d273b30f1f40
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-05-03 15:41:53 +0200

    Graphic.cpp/hpp: New system with new apperance

--- gui/Graphic.cpp
+++ gui/Graphic.cpp
@@ -8,16 +8,24 @@
 #include <iostream>
 #include "Graphic.hpp"
 
-gfx::Graphic::Graphic() : m_inputBox(Gtk::ORIENTATION_VERTICAL),
-			  m_buttonBox(Gtk::ORIENTATION_HORIZONTAL),
+gfx::Graphic::Graphic() : m_principalBox(Gtk::ORIENTATION_VERTICAL),
 			  	m_chooseFile("Choose File"),
-				m_validate("Validate")
-
+				m_Close("Close"),
+			  m_buttonIpAddr("IP_ADDR"),
+			  m_buttonPhone("PHONE_NUMBER"),
+			  m_buttonEmail("EMAIL_ADDR"),
+			  m_LeftFrame("Chosen Files"),
+			  m_RightFrame("Process")
 {
 	setWindow();
-	add(m_inputBox);
+	m_secondBox[0].set_orientation(Gtk::ORIENTATION_VERTICAL);
+	m_secondBox[1].set_orientation(Gtk::ORIENTATION_VERTICAL);
+	DisplayBox.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
+	add(m_principalBox);
 	setBoxInputCmdLine();
 	show_all_children();
+	m_secondBox[0].hide();
+	m_secondBox[1].hide();
 }
 
 gfx::Graphic::~Graphic()
@@ -28,51 +36,92 @@ void gfx::Graphic::setWindow()
 {
 	set_default_size(1280, 720);
 	set_border_width(10);
+	m_LabelTittle.set_markup("<b>Selected Files</b>");
 	set_title("plazza");
 	set_role("plazza");
 }
 
+void gfx::Graphic::setBoxLeft()
+{
+	m_secondBox[0].pack_start(m_thirdBox[0]);
+	m_secondBox[0].pack_end(m_thirdBox[1]);
+	m_thirdBox[0].pack_start(m_LeftFrame);
+	m_LeftFrame.add(m_LeftLabel);
+	m_LeftFrame.set_label_align(0.01, 0.5);
+	m_buttonIpAddr.signal_clicked().connect(sigc::mem_fun(*this,
+							      &Graphic::onButtonShowProcess));
+	m_buttonPhone.signal_clicked().connect(sigc::mem_fun(*this,
+							      &Graphic::onButtonShowProcess));
+	m_buttonEmail.signal_clicked().connect(sigc::mem_fun(*this,
+							      &Graphic::onButtonShowProcess));
+	m_buttonIpAddr.set_size_request(10,10);
+	m_buttonPhone.set_size_request(10,10);
+	m_buttonEmail.set_size_request(10,10);
+	m_thirdBox[1].pack_start(m_buttonIpAddr);
+	m_thirdBox[1].pack_start(m_buttonPhone);
+	m_thirdBox[1].pack_start(m_buttonEmail);
+}
+
 void gfx::Graphic::setBoxInputCmdLine()
 {
+	m_principalBox.pack_start(m_LabelTittle, Gtk::PACK_SHRINK);
+	m_principalBox.pack_start(DisplayBox, Gtk::PACK_EXPAND_WIDGET);
+	m_principalBox.pack_end(m_buttonBox, Gtk::PACK_SHRINK);
+	DisplayBox.pack_start(m_secondBox[0], Gtk::PACK_EXPAND_WIDGET);
+	DisplayBox.pack_start(m_secondBox[1], Gtk::PACK_EXPAND_WIDGET);
+	m_buttonBox.pack_start(m_Close);
 	m_buttonBox.pack_start(m_chooseFile);
-	m_buttonBox.pack_start(m_validate);
-	m_inputBox.pack_end(m_buttonBox, Gtk::PACK_SHRINK);
-	m_validate.signal_clicked().connect(sigc::mem_fun(*this,
+	setBoxLeft();
+	m_secondBox[1].pack_start(m_RightFrame);
+	m_Close.signal_clicked().connect(sigc::mem_fun(*this,
 							&Graphic::onButtonClicked));
 	m_chooseFile.signal_clicked().connect(sigc::mem_fun(*this,
-							  &Graphic::onChooseFile));
-	m_validate.set_size_request(10, 10);
-	m_validate.show();
+							&Graphic::onChooseFile));
+	m_RightFrame.set_label_align(0.02, 0.5);
+	m_Close.set_size_request(10, 10);
+	m_Close.show();
 	m_chooseFile.show();
 }
 
 void gfx::Graphic::onButtonClicked()
 {
+	hide();
+}
+
+void gfx::Graphic::onButtonShowProcess()
+{
+	selectedFiles.clear();
 	std::cout << "Start Process" << std::endl;
 }
 
+
 void gfx::Graphic::selectFile()
 {
-	std::cout << "Selected file" << std::endl;
+	m_secondBox[1].show();
+	m_secondBox[0].show();
 }
 
 void gfx::Graphic::onChooseFile()
 {
-	Gtk::FileChooserDialog dialog("Please choose a folder",
+	std::string filesName;
+	Gtk::FileChooserDialog dialog("Please choose files",
 				      Gtk::FILE_CHOOSER_ACTION_OPEN);
 	dialog.set_select_multiple();
+	filesName.clear();
+	m_LeftLabel.set_text("");
 	dialog.set_transient_for(*this);
 	dialog.add_button("Cancel", Gtk::RESPONSE_CANCEL);
 	Gtk::Button *select = dialog.add_button("Select", Gtk::RESPONSE_OK);
 	select->signal_clicked().connect(sigc::mem_fun(*this,
-							    &Graphic::selectFile));
+							&Graphic::selectFile));
 	int result = dialog.run();
 	if (result == Gtk::RESPONSE_OK) {
 		auto allFileName = dialog.get_filenames();
 		for (unsigned int i = 0; i < allFileName.size(); i++) {
-			std::cout << "File selected: " << allFileName[i]
-				  << std::endl;
+			filesName += allFileName[i] + "\n";
+			selectedFiles.push_back(allFileName[i]);
 		}
+		m_LeftLabel.set_text(m_LeftLabel.get_text() + filesName);
 	}
 }
 
--- include/Graphic.hpp
+++ include/Graphic.hpp
@@ -8,6 +8,7 @@
 #ifndef PLAZZA_GRAPHIC_HPP
 	#define PLAZZA_GRAPHIC_HPP
 
+	#include <array>
 	#include <gtkmm.h>
 	#include <gtkmm/button.h>
 	#include <gtkmm/window.h>
@@ -24,18 +25,36 @@
 			virtual void onButtonClicked();
 			virtual void onChooseFile();
 			virtual void selectFile();
+			virtual void onButtonShowProcess();
 
 		protected:
 
 			plazza::InfoType typeLastFile;
-			Gtk::Box m_inputBox;
+			Gtk::Box m_principalBox;
+
+
+			std::array<Gtk::Box, 2> m_secondBox;
+			std::array<Gtk::Box, 2> m_thirdBox;
+			Gtk::Box DisplayBox;
 			Gtk::Box m_buttonBox;
+
 			Gtk::Button m_chooseFile;
-			Gtk::Button m_validate;
+			Gtk::Button m_Close;
+			Gtk::Button m_buttonIpAddr;
+			Gtk::Button m_buttonPhone;
+			Gtk::Button m_buttonEmail;
+
+			Gtk::Frame m_LeftFrame;
+			Gtk::Frame m_RightFrame;
+			Gtk::Label m_LabelTittle;
+			Gtk::Label m_LeftLabel;
+			Gtk::Label m_RightLabel;
 
 		private:
+			std::vector<std::string> selectedFiles;
 			void setWindow();
 			void setBoxInputCmdLine();
+			void setBoxLeft();
 		};
 	}
 
